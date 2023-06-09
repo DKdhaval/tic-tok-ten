138c484c3f4b8fa931653c972f1d97a32ce1e549
dk347 <134257449+dk347@users.noreply.github.com>
2023-06-09 22:11:29 +0530

    my commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Tic  from './Tic';
+function App() {
+  return (
+    <div className="App">
+      
+     <Tic/>
+
+    </div>
+  );
+}
+
+export default App;
